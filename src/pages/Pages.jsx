b927293e323a0c8d97648931dce175a1348e669e
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-06-08 22:19:42 +0530

    i lost counting - Break

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,16 @@
 import Home from "./Home";
 import Cuisine from "./Cuisine";
 import { Route, Routes } from "react-router-dom";
+import Recipe from "./Recipe";
+import Searched from "./Searched";
 
 function Pages() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/cuisine/:type" element={<Cuisine />} />
+      <Route path="/searched/:search" element={<Searched />} />
+      <Route path="/recipe/:id" element={<Recipe />} />
     </Routes>
   );
 }
