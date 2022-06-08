5a02a3cfaa44cc212d179f4703bdf05f8ed86ef3
notharshprajapati <prajapati.harsh0243@gmail.com>
2022-06-08 19:33:14 +0530

    2nd Break

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,4 +1,8 @@
 import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+import "@splidejs/react-splide/css";
+
 function Popular() {
   const [popular, setPopular] = useState([]);
 
@@ -15,16 +19,46 @@ function Popular() {
   };
 
   return (
-    <div> 
-      {popular.map((recipe) => {
-        return (
-          <div key={recipe.id}>
-            <p>{recipe.title}</p>
-          </div>
-        );
-      })}
+    <div>
+      <Wrapper>
+        <h3>Popular Picks</h3>
+
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          {popular.map((recipe) => {
+            return (
+              <SplideSlide>
+                <Card>
+                  <p>{recipe.title}</p>
+                  <img src={recipe.image} alt={recipe.title} />
+                </Card>
+              </SplideSlide>
+            );
+          })}
+        </Splide>
+      </Wrapper>
     </div>
   );
 }
+
+const Wrapper = styled.div`
+  margin: 4rem 0rem;
+`;
+const Card = styled.div`
+  min-height: 25rem;
+  border-radius: 2rem;
+  overflow: hideen;
+
+  img {
+    border-radius: 2rem;
+  }
+`;
 
 export default Popular;
